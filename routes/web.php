0233bdc66ae09feb3b57995af7f1fc650596e96b
Sirley <szuniga16@misena.edu.co>
2021-12-01 19:25:52 -0500

    Modifiqué rutas de factura y detalle factura

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\CreditoController;
 use App\Http\Controllers\PagoController;
+use App\Http\Controllers\FacturaController;
+use App\Http\Controllers\DetFacturaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -33,5 +35,16 @@ Route::get('/api/pago', [PagoController::class, 'index']);
 Route::post('/api/pago/registrar', [PagoController::class, 'store']);
 Route::put('/api/pago/actualizar', [PagoController::class, 'update']);
 Route::delete('/api/pago/eliminar', [PagoController::class, 'destroy']);
+
+/* Rutas Factura */
+Route::get('api/factura/', [FacturaController::class, 'index']);
+Route::post('api/factura/registrar', [FacturaController::class, 'store']);
+Route::put('api/factura/actualizar', [FacturaController::class, 'update']);
+Route::delete('api/factura/eliminar', [FacturaController::class, 'destroy']);
+
+/* Routes Detalle Factura */
+Route::get('api/detFactura/', [DetFacturaController::class, 'index']);
+Route::put('api/detFactura/actualizar', [DetFacturaController::class, 'update']);
+Route::delete('api/detFactura/eliminar', [DetFacturaController::class, 'destroy']);
 
 
