592163387771847ca8a9f2e4061ad1950f87658b
bryan <bryanhcmrap@gmail.com>
2021-12-17 15:51:45 -0500

    controladores

--- app/Http/Controllers/RangoController.php
+++ app/Http/Controllers/RangoController.php
@@ -6,5 +6,40 @@ use Illuminate\Http\Request;
 
 class RangoController extends Controller
 {
+
+    public function index(){
+
+        $rango = rango::get();
+        return[
+            'Rangos'=> $rango
+        ];
+    }
+
+
+
+    public function store (Request $request){
+         $rango = new rango();
+         $rango-> nombre = $request-> nombre;
+         $rango-> cantidad = $request-> cantidad;
+         $rango->save();
+
+    }
+
+    public function update(Request $request) {
+        $rango = Rango::findOrFail($request->id);
+        $rango->nombre = $request->nombre;
+        $rango->cantidad = $request->cantidad;
+
+
+        $rango->save();
+        
+    }
+
+    public function delete(Request $request) {
+        $rango = Rango::findOrFail($request->id);    
+        $rango->delete();
+    }
+
     //
 }
+
